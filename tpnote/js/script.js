cf80a129ecfb5556377a1afb4ad5c4f377002276
steph <stephane.abecassis@gmail.com>
2017-10-18 16:18:07 +0200

    tp

---
+++ tpnote/js/script.js
@@ -0,0 +1,37 @@
+function verifNom(){
+
+
+
+}
+
+function verifPrenom(){
+
+
+
+
+}
+
+
+
+function verifEmail() {
+
+
+//penser à vérifier si l'email est unique
+
+}
+
+
+function inserTableau() {
+//parcourir en jquery le nombre de lignes, et après la dernière, insérer une nouvelle ligne
+console.log("coucou");
+return false;
+
+
+
+
+
+
+}
+
+
+//tableau = 
